4af2fd46c6b2836c35ccf118cb343ef29442e542
Dóra Gőgh <g.dori2012@gmail.com>
2021-10-16 18:07:14 +0200

    exercise 3

--- TDA4ES_5Het_IRF/VaR_mutato/Form1.cs
+++ TDA4ES_5Het_IRF/VaR_mutato/Form1.cs
@@ -12,7 +12,7 @@ namespace VaR_mutato
 {
     public partial class Form1 : Form
     {
-        
+        PortfolioEntities context = new PortfolioEntities();
         
         public Form1()
         {
