e3a44dc63f17b6bd1aaffb54b0857af3930e53a9
Dóra Gőgh <g.dori2012@gmail.com>
2021-10-28 21:00:22 +0200

    exercise 2-3

--- 6Het_TDA4ES_IRF/ArfolyamValtozas/Form1.cs
+++ 6Het_TDA4ES_IRF/ArfolyamValtozas/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArfolyamValtozas.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,26 @@ namespace ArfolyamValtozas
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
-            InitializeComponent();
+            //InitializeComponent();
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
+            // Ezért a response változó valójában GetExchangeRatesResponseBody típusú.
+            var response = mnbService.GetExchangeRates(request);
+
+            // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
+            // Ezért a result változó valójában string típusú.
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
