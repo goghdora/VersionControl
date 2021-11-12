9b5beb47900095d5d964fce6ec147fcfde7fade6
Dóra Gőgh <g.dori2012@gmail.com>
2021-11-12 14:40:41 +0100

    ball + factory

---
+++ 8Het_TDA4ES_GoghDora_IRF/Ajándék futószalag/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajándék_futószalag.Entities
+{
+    public class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
