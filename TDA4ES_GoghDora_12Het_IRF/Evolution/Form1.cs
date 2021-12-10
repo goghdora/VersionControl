cdc554158ef853d6e6d0b9dc5192eecd78439a96
Dóra Gőgh <g.dori2012@gmail.com>
2021-12-10 23:12:29 +0100

    winner

--- TDA4ES_GoghDora_12Het_IRF/Evolution/Form1.cs
+++ TDA4ES_GoghDora_12Het_IRF/Evolution/Form1.cs
@@ -21,6 +21,8 @@ namespace Evolution
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
+        Brain winnerBrain = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -36,10 +38,7 @@ namespace Evolution
             }
             gc.Start();
 
-            var playerList = from p in gc.GetCurrentPlayers()
-                             orderby p.GetFitness() descending
-                             select p;
-            var topPerformers = playerList.Take(populationSize / 2).ToList();
+            
         }
 
         private void Gc_GameOver(object sender)
@@ -49,6 +48,37 @@ namespace Evolution
                 "{0}. generáció",
                 generation);
             label1.BringToFront();
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
